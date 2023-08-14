c3451957e123b120e8ec73a290bc18b19b029277
Alexey Ivannikov <ivannikov495@gmail.com>
2023-08-15 00:52:35 +0600

    created simple presentation

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -9,6 +9,13 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private var autoIncrementId = 0
 
+    init {
+        for (i in 0..10){
+            val item = ShopItem("$i", i, true)
+            addShopItem(item)
+        }
+    }
+
     override fun getShopItem(shopItemId: Int): ShopItem {
         return shopList.find { it.id == shopItemId }
             ?: throw RuntimeException("Element with id $shopItemId not found")
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        viewModel.shopList.observe(this){
+            Log.d("TEST_LOAD_DATA", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
